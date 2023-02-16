d8bee099852b30bf7db377a61e03d2375b0a9591
mohamed shames <mohamedhames900@gmail.com>
2023-02-16 19:34:08 +0200

    update

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-from optparse import OptionParser
+from optparse import *
 
 parser = OptionParser("""
     My Tool is designed for pentesting
@@ -8,11 +8,18 @@ parser = OptionParser("""
 """)
 parser.add_option("-u","--url",dest="target_url",help="the website")
 parser.add_option("-d","--data",dest="target_data",help="the website data")
-parser.add_option("-U","--username",dest="target_username",help="username")
-parser.add_option("-P","--password",dest="target_password",help="password")
+
+#----------- Advanced Group --------------------
+
+new_group = OptionGroup(parser,"advanced option group", "some options for advanced usage")
+new_group.add_option("-p","--payload",dest="target_payload",help="the payload", default="something went wrong!")
+new_group.add_option("-D","--downlaod",dest="target_download",help="the download", default="something went wrong!")
+parser.add_option_group(new_group)
+
 (options, args) = parser.parse_args()
-if options.target_url == None and options.target_data == None and options.target_username == None and options.target_password == None:
+if options.target_url == None and options.target_data == None:
     print(parser.usage)
     exit(0)
 else:
-    print("{0}:{1}:{2}:{3}".format(options.target_url, options.target_data, options.target_username, options.target_password))
+    print("{0}:{1}".format(options.target_url, options.target_data))
+    print("{0}:{1}".format(options.target_payload, options.target_download))
