8e69503b396fdaafa9e33786af241d1bb5d56624
mohamed shames <mohamedhames900@gmail.com>
2023-02-16 16:06:24 +0200

    update

--- script.py
+++ script.py
@@ -6,10 +6,10 @@ parser = OptionParser("""
     --help for more information
          **BY Mohamed shams**                       
 """)
-parser.add_option("-u","--url",dest="target_url",help="the website", default="www.facebook.com/")
-parser.add_option("-d","--data",dest="target_data",help="the website data",default="my data")
-parser.add_option("-U","--username",dest="target_username",help="username",default="username")
-parser.add_option("-P","--password",dest="target_password",help="password",default="password")
+parser.add_option("-u","--url",dest="target_url",help="the website")
+parser.add_option("-d","--data",dest="target_data",help="the website data")
+parser.add_option("-U","--username",dest="target_username",help="username")
+parser.add_option("-P","--password",dest="target_password",help="password")
 (options, args) = parser.parse_args()
 if options.target_url == None and options.target_data == None and options.target_username == None and options.target_password == None:
     print(parser.usage)
